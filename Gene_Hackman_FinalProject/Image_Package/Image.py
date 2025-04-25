ee7c4dad6906cf8c8337c8e2a207dfb8a66f49e2
Shantele King <Shantele.kingsch@gmail.com>
2025-04-25 14:30:12 -0400

    Added code for photo to Image.py

--- Gene_Hackman_FinalProject/Image_Package/Image.py
+++ Gene_Hackman_FinalProject/Image_Package/Image.py
@@ -1,1 +1,21 @@
 
+from PIL import Image
+import os
+
+def load_Photo():
+    """  
+    Loads an image file named 'FinalPhoto.jpg' from the '../FinalPhoto/' directory 
+
+    @param: None
+
+    @return: Final Photo image taken with quote from Hoosier.
+    """
+    # Get the absolute path of the image
+    image_path = os.path.join(os.path.dirname(__file__), "../FinalPhoto/FinalPhoto.jpg")
+    
+    try:
+        img = Image.open(image_path)
+        return img
+    except FileNotFoundError:
+        print(f"Error: Image file not found at {image_path}")
+        return None
--- Gene_Hackman_FinalProject/Main_Package/Main.py
+++ Gene_Hackman_FinalProject/Main_Package/Main.py
@@ -1,4 +1,13 @@
+from Image_Package.Image import load_Photo
 
 
 
 
+
+if __name__ == "__main__":
+
+    image = load_Photo()
+
+if image:
+    # Display the image using the default image viewer
+    image.show()
